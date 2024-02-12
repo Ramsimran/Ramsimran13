abd65728a0c5ec9701e1578caa19c619a612a19d
Ramsimran <159825474+Ramsimran@users.noreply.github.com>
2024-02-13 00:21:50 +0400

    Add files via upload

--- NumberGuessingGame.py
+++ NumberGuessingGame.py
@@ -6,7 +6,6 @@ class GuessingGame(object):
         self.numberOfGuesses =numberOfGuesses
     def guessNumber(self,attemptsLeft):
             userInput = input("Guess a number: ")
-
             if int(userInput)>self.lowerBound and int(userInput)<self.upperBound:
                 print("Number guessed by a user is: ",userInput)
                 return userInput
