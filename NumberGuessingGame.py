4247440002e6f80644daf584314e5766d7541d85
Ramsimran <159825474+Ramsimran@users.noreply.github.com>
2024-02-13 00:13:43 +0400

    Add files via upload

--- NumberGuessingGame.py
+++ NumberGuessingGame.py
@@ -4,18 +4,20 @@ class GuessingGame(object):
         self.lowerBound = lowerBound
         self.upperBound = upperBound
         self.numberOfGuesses =numberOfGuesses
-    def guessNumber(self):
+    def guessNumber(self,attemptsLeft):
             userInput = input("Guess a number: ")
+
             if int(userInput)>self.lowerBound and int(userInput)<self.upperBound:
                 print("Number guessed by a user is: ",userInput)
                 return userInput
             else:
-                print("Wrong number, please try again as number of chances left are: ",self.numberOfGuesses)
+                print("Wrong number, please try again as number of chances left are: ",attemptsLeft)
                 return 0
     def startGame(self):
-        for i in range(1,self.numberOfGuesses):
+        for i in range(1,self.numberOfGuesses+1):
+            attemptsLeft = self.numberOfGuesses - i
             #invoke guessNumber method
-            userInput = self.guessNumber()
+            userInput = self.guessNumber(attemptsLeft)
             if userInput == 0:
                 continue
             #Generate a random number
@@ -24,9 +26,8 @@ class GuessingGame(object):
             if randomNumer == int(userInput):
                 print("Congratulations!")
             else:
-                print("You lost the attempt, try again. ")
+                print("You lost the attempt no.",attemptsLeft, "try again. ")
 #create Object
-game = GuessingGame(1,10,3)
+#change input parameter as req.
+game = GuessingGame(1,5,2)
 game.startGame()
-
-
